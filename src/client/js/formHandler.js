8c9a779904c960b68c6e5b6489d0c510b5701e73
coding-spidey <coding-spidey@users.noreply.github.com>
2020-04-27 16:53:09 +0530

    Moved from server side to client side

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,23 +1,60 @@
+import { request } from "http";
+let countryName
 function handleSubmit(event) {
     event.preventDefault()
     const cityName = document.getElementById('cityName').value;
     const date = document.getElementById('date').value;
     // console.log(travelDate, cityName)
-    let main = document.querySelector('.main')
-    
+    const main = document.querySelector('.main')
+    console.log('testing')
 
-    fetch('http://localhost:8081/data',
-    {
-        method: 'POST',
-        mode:'cors',
-        body: JSON.stringify({date, cityName}),
-        headers: {"Content-Type": "application/json"}
-    })
-    .then(res => res.json())
-    .then(function(res) {
+    fetch('http://localhost:8081/credentails')
+    .then(req=>req.json())
+    .then(async function(req) {
+        const cordUsername = req.cordUsername
+        const weatherKey = req.weatherKey
+        const pixabayKey = req.pixabayKey
+        const weatherData = await extractCoordinate(cityName, date, cordUsername, weatherKey)
+        const imgUrl = await getImage(cityName, pixabayKey)
+        console.log("---Updating UI---")
         main.innerHTML = "<div class='card'><div class='img'></div><div class='info'></div></div>"
-        document.querySelector('.img').innerHTML = `<img src=${res.imgUrl}/>`
-        document.querySelector('.info').innerHTML = `<h4>${cityName},${res.countryName}</h4><br><p>Your travel date is : ${date}<br/>The weather during your travel will be ${res.description}, and the temperature would be ${res.temp}° Celsius</p>`
+        document.querySelector('.img').innerHTML = `<img src=${imgUrl}/>`
+        document.querySelector('.info').innerHTML = `<h4>${cityName}, ${countryName}</h4><br><p>Your travel date is : ${date}<br/>The weather during your travel will be ${weatherData.description}, and the temperature would be ${weatherData.temp}° Celsius</p>`
     })
+}
+async function extractCoordinate(destination, travelDate, cordUsername, weatherKey) {
+    const coordinateLink =`http://api.geonames.org/searchJSON?q=${destination}&maxRows=1&username=${cordUsername}`
+    const cordData = await fetch(coordinateLink)
+    const json = await cordData.json()
+    const lat = json.geonames[0].lat
+    const lon = json.geonames[0].lng
+    countryName = json.geonames[0].countryName
+    const temp = (await getWeather(lat, lon, travelDate, weatherKey)).temp
+    const description = (await getWeather(lat, lon, travelDate, weatherKey)).description
+    return {
+        temp,
+        description
+    }
+}
+async function getWeather(lat, lon, travelDate, weatherKey) {
+    const resp = await fetch(`https://api.weatherbit.io/v2.0/current?&lat=${lat}&lon=${lon}&key=${weatherKey}&start_date=${travelDate}`)
+    const weatherData = await resp.json()
+    const temp = weatherData.data[0].temp
+    const description = weatherData.data[0].weather.description
+    return{
+        temp,
+        description
+    }
+}
+async function getImage(destination, pixabayKey) {
+    const resp = await fetch(`https://pixabay.com/api/?key=${pixabayKey}&q=${destination}&image_type=photo`)
+    const json = await resp.json()
+    const hits = json.hits
+    if(hits.length==0) {
+        const resp = await fetch(`https://pixabay.com/api/?key=${pixabayKey}&q=${countryName}&image_type=photo`)
+        const json = await resp.json()
+    }
+    const imgUrl = json.hits[0].webformatURL
+    return(imgUrl)
 }
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 projectData = {}
 
-const dotenv = require("dotenv").config({ path: './src/server/.env' })
+require("dotenv").config({ path: './src/server/.env' })
   
 var path = require('path')
 const express = require('express')
@@ -24,50 +24,16 @@ app.listen(8081, function () {
    console.log('Example app listening on port 8081!')
 })
 
-let countryName
+const cordUsername = process.env.cordUsername
+const weatherKey = process.env.WEATHER_KEY
+const pixabayKey = process.env.PIXABAY_KEY
 
-
-async function extractCoordinate(destination, travelDate) {
-    const cordUsername = process.env.cordUsername
-    const coordinateLink =`http://api.geonames.org/searchJSON?q=${destination}&maxRows=1&username=${cordUsername}`
-    const cordData = await fetch(coordinateLink)
-    const json = await cordData.json()
-    const lat = json.geonames[0].lat
-    const lon = json.geonames[0].lng
-    countryName = json.geonames[0].countryName
-    projectData.countryName = countryName
-    await getWeather(lat, lon, travelDate)
+app.get('/credentails', sendData)
+function sendData(req, res) {
+    console.log("---sending credentials---")
+    res.send({
+        cordUsername,
+        weatherKey,
+        pixabayKey
+    })
 }
-async function getWeather(lat, lon, travelDate) {
-    const weatherKey = process.env.WEATHER_KEY
-    const resp = await fetch(`https://api.weatherbit.io/v2.0/current?&lat=${lat}&lon=${lon}&key=${weatherKey}&start_date=${travelDate}`)
-    const weatherData = await resp.json()
-    const temp = weatherData.data[0].temp
-    const description = weatherData.data[0].weather.description
-    projectData.temp = temp
-    projectData.description = description
-}
-async function getImage(destination, countryName) {
-    const pixabayKey = process.env.PIXABAY_KEY
-    const resp = await fetch(`https://pixabay.com/api/?key=${pixabayKey}&q=${destination}&image_type=photo`)
-    const json = await resp.json()
-    const hits = json.hits
-    if(hits.length==0) {
-        const resp = await fetch(`https://pixabay.com/api/?key=${pixabayKey}&q=${countryName}&image_type=photo`)
-        const json = await resp.json()
-    }
-    const imgUrl = json.hits[0].webformatURL
-    projectData.imgUrl = imgUrl
-}
-app.post('/data',async (req,res)=>{
-    const travelDate = req.body.date
-    const destination = req.body.cityName
-    await extractCoordinate(destination, travelDate)
-    await getImage(destination, countryName)
-    try {
-        console.log("Sending data");
-        res.send(projectData)
-    } catch(error) {
-      console.log(error);
-    }
-})
